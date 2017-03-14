82cdabad638910491526ccc9fbf9e7a557e1c480
Filip <Filip.Thyssen@cern.ch>
2017-03-14 15:19:22 +0100

    change filenames to angleScale_RPC_CPPF??.txt

--- test/RPCEMTFPhiThetaScale_sort.sh
+++ test/RPCEMTFPhiThetaScale_sort.sh
@@ -2,7 +2,7 @@
 
 for sector in 1 2 3 4; do
     board=$((sector))
-    sort -k1,1 -k8,8n -o CPPF_p${sector}.txt RPCAMCLink_1386_${board}.txt
+    sort -k1,1 -k8,8n -o angleScale_RPC_CPPFp${sector}.txt RPCAMCLink_1386_${board}.txt
     board=$((sector + 4))
-    sort -k1,1 -k8,8n -o CPPF_n${sector}.txt RPCAMCLink_1386_${board}.txt
+    sort -k1,1 -k8,8n -o angleScale_RPC_CPPFn${sector}.txt RPCAMCLink_1386_${board}.txt
 done;
